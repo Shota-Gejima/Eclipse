8fd6c54aa472a0240f916caeca7ddd6753f637ee
Shota-Gejima <bewith.sho@gmail.com>
2023-12-19 18:53:13 +0900

    [add]for

--- firstEclipse/src/firstEclipse/Main.java
+++ firstEclipse/src/firstEclipse/Main.java
@@ -2,9 +2,10 @@
 
 public class Main {
 	public static void main(String[] args) {
-		double a = 9.0; int b = 2;
-		double c = a/b;
-		System.out.println("縦幅3横幅5の長方形の面積は、"+ c);
-		System.out.println("私の好きな記号は(\")です");
+	
+		for (int i=0; i<5; i++) {
+			System.out.println("Hello");
 		}
+		
+	}
 }
--- firstEclipse/src/firstEclipse/lesson2.java
+++ firstEclipse/src/firstEclipse/lesson2.java
@@ -9,10 +9,22 @@ public class lesson2 {
 		System.out.println("あなたの年齢を入力してください");
 		String ageString = new java.util.Scanner(System.in).nextLine();
 		int age = Integer.parseInt(ageString);
-		int fortune = new java.util.Random().nextInt(4);
+		int fortune = new java.util.Random().nextInt(3);
 		fortune++;
-		System.out.println("占いの結果がでました！");
-		System.out.println(age+"歳の"+name+"さん、あなたはの運気番号は"+fortune+"です");
-		System.out.println("1: 大吉 2: 中吉: 3: 吉 4: 凶");
+		System.out.println("占いの結果がでました");
+		
+		switch (fortune) {
+		case 1:
+			System.out.println("大吉です");
+			break;
+		case 2:
+			System.out.println("吉です");
+			break;
+		case 3:
+			System.out.println("末吉です");
+			break;
+		case 4:
+			System.out.println("凶です");
+		}
 	}
 }
