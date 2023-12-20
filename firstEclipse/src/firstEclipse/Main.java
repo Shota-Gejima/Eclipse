8e89ccdfa24c8bdb3c005703cbe771c4c9f28660
Shota-Gejima <bewith.sho@gmail.com>
2023-12-20 10:31:29 +0900

    [finish]lesson3

--- firstEclipse/src/firstEclipse/Main.java
+++ firstEclipse/src/firstEclipse/Main.java
@@ -2,10 +2,12 @@
 
 public class Main {
 	public static void main(String[] args) {
-	
-		for (int i=0; i<5; i++) {
-			System.out.println("Hello");
+		for (int i=1; i<10; i++) {
+			for (int n=1; n<10; n++) {
+				System.out.print(i*n);
+				System.out.print(" ");
+			}
+			System.out.println("");
 		}
-		
 	}
 }
--- firstEclipse/src/firstEclipse/lesson2.java
+++ firstEclipse/src/firstEclipse/lesson2.java
@@ -3,28 +3,21 @@ package firstEclipse;
 public class lesson2 {
 
 	public static void main(String[] args) {
-		System.out.println("ようこそ占いの館へ");
-		System.out.println("あなたの名前を入力してください");
-		String name = new java.util.Scanner(System.in).nextLine();
-		System.out.println("あなたの年齢を入力してください");
-		String ageString = new java.util.Scanner(System.in).nextLine();
-		int age = Integer.parseInt(ageString);
-		int fortune = new java.util.Random().nextInt(3);
-		fortune++;
-		System.out.println("占いの結果がでました");
-		
-		switch (fortune) {
-		case 1:
-			System.out.println("大吉です");
-			break;
-		case 2:
-			System.out.println("吉です");
-			break;
-		case 3:
-			System.out.println("末吉です");
-			break;
-		case 4:
-			System.out.println("凶です");
+		System.out.println("数あてゲームをします");
+		int ans = new java.util.Random().nextInt(10);
+		for (int i=0; i<5; i++) {
+			System.out.println("0～9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (ans == num) {
+				System.out.println("あたりです");
+				break;
+			} else {
+				System.out.println("違います");
+				if (i==4) {
+					System.out.println("答えは"+ans+"です");
+				}
+			}
 		}
+		System.out.println("終了します");
 	}
 }
